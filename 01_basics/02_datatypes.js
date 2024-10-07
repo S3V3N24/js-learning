de74d0176d8864181dc45065d3b483b851932e86
S3V3N24 <work.aps7@gmail.com>
2024-10-07 16:02:50 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,3 +3,23 @@
 //alert(3+3) we are using node js
 
 //code readibllity is important.
+
+let name = "aayush"
+let age = 20
+let isLoggedIn = false
+
+//number => 2 ratio to power 52
+//bigint
+//string use ""
+//boolean true/false
+//null => stand alonevalue
+//undefined => 
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
