6728d65c35fb9c515c3871545a3cdca21d2c34f5
S3V3N24 <work.aps7@gmail.com>
2024-10-08 10:39:58 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,31 @@
-let score = "69"
+//let score = true
 
-console.log(typeof score)
+//console.log(typeof score);
+//console.log(typeof (score)); //both are the methods on finding the type,
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); 
+
+
+/*
+"69" => 69
+"69xx" => NaN Not a number.
+true => 1 , false => 0
+*/
+
+//let isLoggedIn = "Aayush"
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false
+//"aayush" => true
+
+let somenumber = 69
+let stringNumber = String(somenumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
