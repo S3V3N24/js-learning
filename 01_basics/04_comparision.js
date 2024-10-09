df03ae97b5a491d11747adfa091b88bad4820ad8
S3V3N24 <work.aps7@gmail.com>
2024-10-09 23:12:55 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1)
+// console.log("02" > 1 )
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined = 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+// use of "==="
+
+// console.log("2"=== 1) //now js checks both datatype and number.
