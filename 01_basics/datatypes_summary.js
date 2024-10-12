04a9d7d7e68fd1691bcb60fb29297bf6a71f3663
S3V3N24 <work.aps7@gmail.com>
2024-10-12 18:24:14 +0000

    datatypes_summary.js

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,30 @@
+//primitive
+
+// 7 types : string, number, boolean, null, undefined, symbol, bigint.
+
+//refenrence (non-primitive)
+
+// const score = 100
+// const scoreValue = 104.6
+
+// let userLoggedIn = false
+// let userEmail;
+
+// const id = Symbol('987')
+// const anotherId = Symbol('987')
+
+// console.log(id === anotherId)
+
+// array, objects, functions 
+
+
+// array => ["xyz"] 
+
+//object 
+// let myobj = {
+//     name: (""),
+//     age : 1,
+// }
+ const myFunction = function(){
+    console.log("Hello World")
+ }
