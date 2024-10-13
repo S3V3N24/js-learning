652a033932a0d599b0b48742da7d16790b380464
S3V3N24 <work.aps7@gmail.com>
2024-10-13 14:10:40 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,19 @@
+// const name = "seven"
+// const repoCount = 60
+
+// console.log(name + repoCount + "value") // this is an old method. do not use.
+
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`) //prefer this method.
+
+const gameName = new String('Seven') //another method for declaring string.
+
+// console.log(gameName[2]); //same as line 14
+
+// console.log(gameName.__prot__); //.__prot__
+
+
+// console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2)); //same as line 10
