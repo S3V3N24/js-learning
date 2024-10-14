2a3a53c2884b96152a51a7037b17269f9885ce5c
Aayush Priyanshu <work.aps7@gmail.com>
2024-10-14 17:00:07 +0000

    string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -5,7 +5,7 @@
 
 // console.log(`hello my name is ${name} and my repo count is ${repoCount}`) //prefer this method.
 
-const gameName = new String('Seven') //another method for declaring string.
+// const gameName = new String('Seven') //another method for declaring string.
 
 // console.log(gameName[2]); //same as line 14
 
@@ -14,6 +14,23 @@ const gameName = new String('Seven') //another method for declaring string.
 
 // console.log(gameName.length);
 
-console.log(gameName.toUpperCase());
+// console.log(gameName.toUpperCase());
 
-console.log(gameName.charAt(2)); //same as line 10
+// console.log(gameName.charAt(2)); //same as line 10
+
+// const newGame = gameName.substring(-4,4)
+// console.log(newGame)  // use to bring letters. couldn't be negative
+
+// const oldGame = gameName.slice(-1,6)
+// console.log(oldGame); //use to bring letters. could bring negative letter.
+
+// const newString = ("     seven    ")
+// console.log(newString);
+// console.log(newString.trim()); //Trim used for removing unwanted spaces between the letters.
+
+const url = 'seven@1234%20.com' //i want to replace %20 so.
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('seven'))// to find if such keywords exist in the string.
+
+console.log(url.includes('89'))
