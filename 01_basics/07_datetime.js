a30fc3150b5b73ff4c35a9853c4cc8411985ebca
Aayush Priyanshu <work.aps7@gmail.com>
2024-10-16 19:52:20 +0000

    date and time

---
+++ 01_basics/07_datetime.js
@@ -0,0 +1,20 @@
+//dates
+
+// let dates = new Date()
+// console.log(dates);
+// console.log(dates.toString());
+// console.log(dates.toDateString());
+// console.log(dates.toLocaleDateString());
+// console.log(dates.toISOString());
+// console.log(dates.toJSON());
+
+// let ownDate = new Date (2023, 0, 15)
+// let ownDate = new Date ("11-04-2024")
+
+// console.log(ownDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+ console.log(myTimeStamp);
+ console.log(myTimeStamp.getTime());
+ 
