43664631cacfa9969b8bda028ac97127daea35d9
Aayush Priyanshu <work.aps7@gmail.com>
2024-10-16 15:18:25 +0000

    num and math

---
+++ 01_basics/06_numandmaths.js
@@ -0,0 +1,46 @@
+// const  number = 300
+// console.log(number);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); //100.00
+
+// const anoNumber = 123.897
+// console.log(anoNumber.toPrecision(2));
+
+// const hundred = 1000000
+// console.log(hundred.toLocaleString()); //in international number system
+
+// console.log(hundred.toLocaleString('en-IN')); //in indian number system
+
+//++++++++++++++ Maths +++++++++++++++++++++++++++
+
+// console.log(Math.abs(-5)); //to change sign from positive to negative.
+
+// console.log(Math.round(5.3)); //to roundoff a value.
+
+// console.log(Math.ceil(5.7)); //always take higher value. 
+
+// console.log(Math.floor(5.4)); //always take lower value
+
+// console.log(Math.min(7,3,6,3)); //will take min value
+
+// console.log(Math.max(7,3,6,3)); //will take max value
+
+// console.log(Math.random()); // to draw random number.
+
+// console.log(Math.random()*10 + 1);
+
+const min = 20
+const max = 50
+
+console.log(Math.floor (Math.random() * (max - min * 1) + min));
+
+
+
+
+
+
+
