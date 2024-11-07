3235ff776df0fa15a3f63ba9ebb6c9f6e0b2e3e0
Aayush Priyanshu <work.aps7@gmail.com>
2024-11-07 19:39:30 +0000

    object

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,3 +1,21 @@
 //const tinderUser = new Object()  //singleton object
-const tinderUser = {} //non singleton object
-console.log(tinderUser);
+// const tinderUser = {} //non singleton object
+// tinderUser.id = "123abc";
+// tinderUser.name = "seven";
+// tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const NewUser = {
+    gmail : "xyz@gmail",
+    fullName : {
+        userFullName : {
+            firstName : "seven",
+            lastName : "eleven",
+        }
+    }
+
+}
+ console.log(NewUser.gmail);
+ 
+
