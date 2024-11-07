071fba98409801c1ce45454ad4736dbc75e06442
Aayush Priyanshu <work.aps7@gmail.com>
2024-11-07 09:44:21 +0000

    object 1

--- 02_basics/02_object.js
+++ 02_basics/02_object.js
@@ -6,6 +6,7 @@ const mySym = Symbol("Key1")
 const JsUser ={
     name : "s3v3n",
     "hello" : "india",
+    [mySym] : "Key1",
     age : 20,
     location : "haryana",
     email : "xyz@gmail.com",
@@ -13,7 +14,24 @@ const JsUser ={
     lastLoggedIn: ["monday", "satureday"]   
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser["hello"]);
-console.log(JsUser[mySym]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["hello"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "seven@xyz"
+// Object.freeze(JsUser) // for freezing any changes.
+JsUser.email = "hello@xyz"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello js User");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello js User, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
