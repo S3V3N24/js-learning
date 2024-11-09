17560a32e79d16b056665ec50d70da839692727f
Aayush Priyanshu <work.aps7@gmail.com>
2024-11-09 22:51:49 +0000

    object

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -6,7 +6,36 @@
 // console.log("n");
 // }
 
+// function addTwoNumbers(number1 , number2){
+// console.log(number1 + number2);
+// }
+//  addTwoNumbers(3,7)  
+
 function addTwoNumbers(number1 , number2){
-console.log(number1 + number2);
+// let result = number1 +number2
+// return number1 + number2
+
+return number1 + number2
+
 }
- addTwoNumbers(3,7)  
+const result = addTwoNumbers(3,7)
+
+
+// console.log("Result: ", result);
+
+function userLoggedin(username){
+    if(username === undefined){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} user is logged in`
+
+}
+
+// console.log(userLoggedin());
+
+function calculateCartPrice(...numb1){ //basic this ... converter it into array.
+    return numb1
+}
+ console.log(calculateCartPrice(200, 300, 600));
+ 
