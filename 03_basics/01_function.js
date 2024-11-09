f7cd51a252457a91061a7288ab2114a942e44343
Aayush Priyanshu <work.aps7@gmail.com>
2024-11-09 14:20:53 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,12 @@
+// function PrintThis(){
+// console.log("s");
+// console.log("e");
+// console.log("v");
+// console.log("e");
+// console.log("n");
+// }
+
+function addTwoNumbers(number1 , number2){
+console.log(number1 + number2);
+}
+ addTwoNumbers(3,7)  
