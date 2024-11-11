333e164ec05aca6eaabe2138726ea296d568d1e3
Aayush Priyanshu <work.aps7@gmail.com>
2024-11-11 23:24:43 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,35 @@
+// const user = {
+//     username: "seven",
+//     price: 99,
+
+// welcomeMessage: function(){
+//     console.log(`${this.user}`, "Welcome to Website");
+//     // console.log(this);
+    
+    
+// }
+// }
+// user.welcomeMessage()
+// user.username = "sam" 
+// user.welcomeMessage()
+// // console.log(this);
+
+// function chai(){
+//     let username = "seven"
+//     console.log(this);
+    
+// }
+
+// chai()
+
+// const chai =  () => {
+//     let username ="seven"
+//     console.log(this);
+// }
+
+// chai()
+
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+console.log(addTwo(3,4))
