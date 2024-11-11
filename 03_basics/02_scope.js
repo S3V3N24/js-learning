612818ec388ec90c36f74d434957a2ae4e813135
Aayush Priyanshu <work.aps7@gmail.com>
2024-11-11 23:24:07 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,49 @@
+var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30
+//    console.log("inner",a);
+   
+}  
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+ 
+// function one() {
+//     const username = "seven"
+     
+//      function two(){
+//         const website = "youtube"
+//         console.log(username);
+        
+//      }
+//     //  console.log(website);
+//     two() 
+// }
+//     one()
+
+// if (true) {
+//     const username = "hitesh"
+//     if (username === "hitesh"){
+//         const website = "youtube"
+//         console.log(username + website);
+        
+//     }
+    
+    
+// }
+
+//+++++++++++++++++++++ Interesting +++++++++++++
+
+function addOne(num) {
+    return num + 1
+}
+addOne()
+
+const addTwo = function(num) {
+    return num + 2
+}
+(addTwo);
