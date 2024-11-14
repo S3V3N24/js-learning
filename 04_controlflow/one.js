d2798f921d69a01c8066796e9c2407789334aeca
Aayush Priyanshu <work.aps7@gmail.com>
2024-11-14 21:43:37 +0000

    one

---
+++ 04_controlflow/one.js
@@ -0,0 +1,28 @@
+//  if statement
+//  >, <, >=, <=, !=, ===(for strict checking), !==.
+
+// const temperature = 40
+
+// if (temperature === 60) {
+//      console.log("temp is below 40");
+// } else {
+//     console.log("temp is above 50");
+    
+// }
+
+//  console.log("executed");
+
+
+// const score = "200"   
+
+// if (score > 200) {      //function scope
+//     let power = "fly"
+//     console.log(`user has power ${power}`);
+    
+// }
+//     console.log(`user has power ${power}`);
+
+const balance = 1000
+
+if(balance > 500) console.log("test");  //implicite scope
+
