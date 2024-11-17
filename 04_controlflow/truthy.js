c416cfb888a849e1c0f734bc075aab8b77b01599
Aayush Priyanshu <work.aps7@gmail.com>
2024-11-17 23:21:34 +0000

    truthy

---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,15 @@
+const userEmail = "zyx@1234"
+
+if (userEmail) {
+    console.log("got user email");
+} else{
+    console.log("no user email");
+    
+}
+
+// false value
+
+// false, 0, -0, BigInt 0n, "", null, underfined, NaN
+
+// truthy value
+// "0", "false", "", [], {}, function{}[]
