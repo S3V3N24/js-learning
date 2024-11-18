30d38e4bb467683ff8d5c6f2399ff1875fea8dd2
Aayush Priyanshu <work.aps7@gmail.com>
2024-11-18 14:21:54 +0000

    truthy

--- 04_controlflow/truthy.js
+++ 04_controlflow/truthy.js
@@ -1,11 +1,11 @@
-const userEmail = "zyx@1234"
+// const userEmail = "zyx@1234"
 
-if (userEmail) {
-    console.log("got user email");
-} else{
-    console.log("no user email");
+// if (userEmail) {
+//     console.log("got user email");
+// } else{
+//     console.log("no user email");
     
-}
+// }
 
 // false value
 
@@ -13,3 +13,40 @@ if (userEmail) {
 
 // truthy value
 // "0", "false", "", [], {}, function{}[]
+
+// const userEmail = "array"
+
+// if (userEmail.length === 0) {
+//    console.log("array is empty");
+    
+// }
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("object is empty");
+    
+// }
+
+// Nullish Coalescing Operator (??): null undefined      
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+val1 = null ?? 5 ?? 10
+
+//Terniary Operator
+// console.log(val1);
+
+// condition ? true : false
+
+const iceTeaPrize = 100
+iceTeaPrize >= 80 ? console.log("less than 80") : console.log("more than 80");
+ 
+
+
+
+
+
+
