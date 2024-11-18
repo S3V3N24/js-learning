abe88f101023498d40f9eb347c4712c7d99186d7
Aayush Priyanshu <work.aps7@gmail.com>
2024-11-18 16:35:50 +0000

    one for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,41 @@
+//for loop
+
+for (let i = 0; i <= 10 ; i++) {
+    const element = i;
+    if (element == 7) {
+        // console.log("seven is the best number");
+        }
+    // console.log(element);
+    
+}
+// project table printing in javascript
+for (let i = 0; i <= 10 ; i++) {
+    // console.log(`Outer loop value: $[i]`);
+    for (let j = 0; j <= 10; j++) {
+        // console.log(i + '*' + j + '=' + i*j );
+        }
+    }
+
+    let myArray = ["batman", "superman", "flash"]
+    // console.log(myArray.length);
+
+    for (let index = 0; index <
+        
+        myArray.length; index++) {
+        const element = myArray[index];
+        // console.log(element);
+    }
+
+    //break and continue
+    
+    for (let i = 1; i <= 20; i++) {
+
+        if (i == 7){
+        console.log(`This is your number ${i}`);
+        // break    
+        continue
+        }
+        console.log(`This is: ${i}`);
+        ;
+        
+    }
