52b48019564d5ea3f318fabe94ec66ba42a82be4
Aayush Priyanshu <work.aps7@gmail.com>
2024-11-20 01:18:07 +0000

    two js

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let myArray = ["bat", "cat", "rat"]
+// let arr = 0
+
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+// let score = 1
+// do {
+//     console.log(`Score is ${score}`);
+//     score = score + 1
+// } while (score <= 10);
