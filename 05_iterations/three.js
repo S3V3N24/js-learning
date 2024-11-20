f3a450dbd1e692ff1ef29a596ea08a4832f42b52
Aayush Priyanshu <work.aps7@gmail.com>
2024-11-20 23:50:37 +0000

    three

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -16,3 +16,26 @@
     
 // }
 
+//Maps
+
+// const map = new Map()
+// map.set('In'  "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('In', "India")
+
+// // console.log(map);  
+
+// for (const [key, value] of map) {
+//     // console.log(key, ':-', value);
+//     }
+
+    const myObject = {   //objects are not iterable
+        'game1' : 'NFS',
+        'game2' : 'Spiderman' 
+    }
+    for (const [key, value] of myObject) {
+        console.log(key, ':-', value);
+        
+    }
+
