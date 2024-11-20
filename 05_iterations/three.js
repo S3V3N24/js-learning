ae5fd460512d1ac25cf449248f2f3c57088962b6
Aayush Priyanshu <work.aps7@gmail.com>
2024-11-20 01:44:16 +0000

    three

---
+++ 05_iterations/three.js
@@ -0,0 +1,18 @@
+//for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//      console.log(num);
+         
+// }
+
+// let greetings = "Hello world!"
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`);
+    
+// }
+
